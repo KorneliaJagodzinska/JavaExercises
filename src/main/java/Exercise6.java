bfd34bda645887de242d090bb81a118a8f7cb360
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-07-24 12:10:00 +0200

    Exercise6

--- src/main/java/Exercise6.java
+++ src/main/java/Exercise6.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+//Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string,
+// the longest possible, containing distinct letters,
+//each taken only once - coming from s1 or s2.
+//Examples:
+//a = "xyaabbbccccdefww"
+//b = "xxxxyyyyabklmopq"
+//longest(a, b) -> "abcdefklmopqwxy"
 
 public class Exercise6 {
+    public static String longest (String s1, String s2) {
+        StringBuilder sb = new StringBuilder();
+        (s1 + s2).chars().distinct().sorted().forEach(c -> sb.append((char) c));
+        return sb.toString();
+    }
 }
--- src/test/java/Exercise6Test.java
+++ src/test/java/Exercise6Test.java
@@ -1,5 +1,15 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Exercise6Test {
+
+    @Test
+    public void test() {
+        System.out.println("longest Fixed Tests");
+        assertEquals("aehrsty", Exercise6.longest("aretheyhere", "yestheyarehere"));
+        assertEquals("abcdefghilnoprstu", Exercise6.longest("loopingisfunbutdangerous", "lessdangerousthancoding"));
+        assertEquals("acefghilmnoprstuy", Exercise6.longest("inmanylanguages", "theresapairoffunctions"));
+    }
 
 }
